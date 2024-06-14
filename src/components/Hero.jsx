9ed2737b17d19bb24315d6ee7e984a01fee8080d
Aryan Jagarwal <aryanjagarwal@Aryans-MacBook-Air.local>
2024-06-14 13:01:45 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,13 @@
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
-import { heroVideo, smallHeroVideo } from "../utils"
-import { useEffect, useState } from "react"
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useEffect, useState } from 'react';
 
 const Hero = () => {
-
   const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
 
   const handleVideoSrcSet = () => {
-    if (window.innerWidth < 760) {
+    if(window.innerWidth < 760) {
       setVideoSrc(smallHeroVideo)
     } else {
       setVideoSrc(heroVideo)
@@ -16,23 +15,16 @@ const Hero = () => {
   }
 
   useEffect(() => {
-    window.addEventListener('resize', handleVideoSrcSet)
+    window.addEventListener('resize', handleVideoSrcSet);
 
     return () => {
-      window.removeEventListener('resize', handleVideoSrcSet)
+      window.removeEventListener('reisze', handleVideoSrcSet)
     }
   }, [])
 
   useGSAP(() => {
-    gsap.to('#hero', {
-      opacity: 1,
-      delay: 2
-    })
-    gsap.to('#cta', {
-      opacity: 1,
-      y: -50,
-      delay: 2
-    })
+    gsap.to('#hero', { opacity: 1, delay: 2 })
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 })
   }, [])
 
   return (
@@ -45,8 +37,12 @@ const Hero = () => {
           </video>
         </div>
       </div>
-      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-        <a href="highlights" className="btn">Buy</a>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">Buy</a>
         <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,18 +1,20 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import React, { useRef, useState } from 'react'
-import ModelView from './ModelView'
-import { yellowImg } from '../utils'
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
+import { yellowImg } from "../utils";
 
-import * as THREE from 'three'
-import { View } from '@react-three/drei'
+import * as THREE from 'three';
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
-
-  const [size, setSize] = useState('small')
+  const [size, setSize] = useState('small');
   const [model, setModel] = useState({
     title: 'iPhone 15 Pro in Natural Titanium',
-    color: ['#8F8A81', '#ffe7b9', '#6f6c64'],
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
     img: yellowImg,
   })
 
@@ -28,20 +30,38 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
+
+    if(size ==='small') {
+      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
 
   useGSAP(() => {
-    gsap.to('#heading', {y: 0, opacity: 1})
-  }, [])
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
 
   return (
-    <section className='common-padding'>
-      <div className='screen-max-width'>
-        <h1 id='heading' className='section-heading'>
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
-        <div className='flex flex-col items-center mt-5'>
-          <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
-            <ModelView
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView 
               index={1}
               groupRef={small}
               gsapType="view1"
@@ -49,18 +69,20 @@ const Model = () => {
               setRotationState={setSmallRotation}
               item={model}
               size={size}
-            />
-            <ModelView
+            />  
+
+            <ModelView 
               index={2}
               groupRef={large}
               gsapType="view2"
               controlRef={cameraControlLarge}
-              setRotationState={setLargeRotation} 
+              setRotationState={setLargeRotation}
               item={model}
               size={size}
             />
-            <canvas
-              className='w-full h-full'
+
+            <Canvas
+              className="w-full h-full"
               style={{
                 position: 'fixed',
                 top: 0,
@@ -72,13 +94,26 @@ const Model = () => {
               eventSource={document.getElementById('root')}
             >
               <View.Port />
-            </canvas>
+            </Canvas>
           </div>
 
-          <div className='mx-auto w-full'>
-            <p className='text-sm font-light text-center mb-5'>{model.title}</p>
-            <div className='flex-center'>
-              // --------------------------------- 2:16:07
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
+                ))}
+              </ul>
+
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                    {label}
+                  </span>
+                ))}
+              </button>
             </div>
           </div>
         </div>
